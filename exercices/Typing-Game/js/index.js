7f17deb529d52118b51e0e320704e1d7475109cb
Ulysse ARNAUD <mexanga@gmail.com>
2020-09-21 19:55:55 +0200

    MAJ de l'exercice TypingGame

--- exercices/Typing-Game/js/index.js
+++ exercices/Typing-Game/js/index.js
@@ -1,3 +1,36 @@
 import {TypingGame} from "./TypingGame/index.js";
 
-const typingGame = new TypingGame;
+document.addEventListener('DOMContentLoaded', event => {
+    const introductionSection = document.querySelector('section[data-name="introduction"]');
+    const links = introductionSection.querySelectorAll('.grid-buttons a');
+    for (
+        let cursor = 0, cursorMax = links.length;
+        cursor < cursorMax;
+        cursor++
+    ) {
+        const link = links[cursor];
+        link.addEventListener('click', event => onClickLink(event));
+    }
+});
+
+function onClickLink(event) {
+    event.preventDefault();
+
+    let {target} = event;
+    const {nodeName} = target;
+    if ('a' !== nodeName.toLowerCase()) target = target.closest('a');
+
+    const {href} = target;
+
+    const introductionSection = document.querySelector('section[data-name="introduction"]');
+    const sectionName = href.substring(href.indexOf("#") + 1);
+
+    const section = document.querySelector(`section[data-name="${sectionName}"]`);
+    introductionSection.classList.remove('show');
+
+    setTimeout(function() {
+        section.classList.add('show');
+    }, 0);
+
+    const typingGame = new TypingGame;
+}
